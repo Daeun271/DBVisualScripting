00a1a3db44978181a309e17274f7f7a0cc8ff42d
Daeun Jung <daeun.jung@ruhr-uni-bochum.de>
2024-11-30 21:27:41 +0100

    logout at frontend completed

--- client/src/app/auth/logout/logout.component.ts
+++ client/src/app/auth/logout/logout.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogRef,
+    MatDialogTitle,
+} from '@angular/material/dialog';
+import { AuthService } from '../../../api/auth.service';
 
 @Component({
     selector: 'app-logout',
-    imports: [],
+    imports: [MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle, MatButtonModule],
     templateUrl: './logout.component.html',
     styleUrl: './logout.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LogoutComponent {}
+
+export class LogoutComponent {
+    readonly dialogRef = inject(MatDialogRef<LogoutComponent>);
+
+    constructor(private authService: AuthService) { }
+
+    logOut(): void {
+        this.authService.logout().subscribe({
+            next: () => {
+                this.authService.logout();
+                this.dialogRef.close();
+            }
+        });
+    }
+}
