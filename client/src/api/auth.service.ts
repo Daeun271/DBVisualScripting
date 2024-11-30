1300cf534806cbf3fe783ac80e979ed41a2c1394
Daeun Jung <daeun.jung@ruhr-uni-bochum.de>
2024-11-30 21:25:50 +0100

    auth api using HttpClient created

---
+++ client/src/api/auth.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { tap, catchError } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    private currentUserSubject = new BehaviorSubject<boolean>(false);
+    public currentUser$ = this.currentUserSubject.asObservable();
+
+    private hostUrl = 'http://localhost:8080/';
+
+    constructor(private http: HttpClient) {
+        this.checkAuthStatus();
+    }
+
+    checkAuthStatus(): void {
+        this.http.get(`${this.hostUrl}auth/status`, { withCredentials: true })
+            .pipe(
+                map(response => !!response),
+                catchError(error => {
+                    if (error.status !== 403) {
+                        throw error;
+                    }
+
+                    this.currentUserSubject.next(false);
+                    return of(false);
+                })
+            )
+            .subscribe(isAuthenticated => this.currentUserSubject.next(isAuthenticated));
+    }
+
+    register(username: string, email: string, password: string): Observable<any> {
+        return this.http.post(`${this.hostUrl}auth/register`, { username, email, password }, { withCredentials: true })
+            .pipe(
+                tap(response => {
+                    this.currentUserSubject.next(true);
+                }),
+                catchError(error => {
+                    this.currentUserSubject.next(false);
+                    throw error;
+                })
+            );
+    }
+
+    login(email: string, password: string): Observable<any> {
+        return this.http.post(`${this.hostUrl}auth/login`, { email, password }, { withCredentials: true })
+            .pipe(
+                tap(response => {
+                    this.currentUserSubject.next(true);
+                }),
+                catchError(error => {
+                    this.currentUserSubject.next(false);
+                    throw error;
+                })
+            );
+    }
+
+    logout(): Observable<any> {
+        return this.http.post(`${this.hostUrl}auth/logout`, {}, { withCredentials: true })
+            .pipe(
+                tap(response => {
+                    this.currentUserSubject.next(false);
+                }),
+                catchError(error => {
+                    this.currentUserSubject.next(false);
+                    throw error;
+                })
+            );
+    }
+}
